55c4f030f97cc6c97c5f69832b3ddfa469362e01
Daria <lvmperouge@gmail.com>
2023-11-24 01:35:57 +0000

    Corrects typo.

--- db_utils.py
+++ db_utils.py
@@ -3,9 +3,10 @@ import yaml
 import pandas as pd
 
 
-with open('credentials.yaml') as file:
-    prime_service = yaml.safe_load(file)
-
+def load_credentials(x):
+    with open(x,'r') as file:
+        return yaml.safe_load(file)
+    
 class RDSDatabaseConnector:
     
     
@@ -19,3 +20,15 @@ class RDSDatabaseConnector:
             conn_str = f"postgresql+psycopg2://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}"
             self.engine = create_engine(conn_str)
         return self.engine
+    
+    def extract_data(self, table_name="loan_payments"):
+        
+        if self.engine is None:
+            self.init_engine()
+        
+        query = f"SELECT * FROM {table_name};"
+        return pd.read_sql_query(query, self.engine)
+
+    def save_df(self, df, filename):
+       
+        return df.to_csv(filename)
